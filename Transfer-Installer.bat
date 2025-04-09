31e2769b95d057635e659a9d9a36275c063c0db0
Vincent <christian289@naver.com>
2025-04-09 18:54:08 +0900

    Update Transfer-Installer.bat
    안되는 이유는 경로가 잘못되었기 때문

--- Transfer-Installer.bat
+++ Transfer-Installer.bat
@@ -1,7 +1,15 @@
 ﻿@echo off
-REM === 결과물 복사 ===
-set "SOLUTION_PATH=%cd%"
-set "TARGET_DIR=\\ad.npixel.co.kr\1_Setup\34_ExcelMerge"
-copy /Y "%SOLUTION_PATH%\ExcelMerge-Setup.exe" "%TARGET_DIR%"
-endlocal
-exit /b 0
+
+set "SOURCE_DIR=%cd%\Build\Release"
+set "FILE_NAME=ExcelMerge-Setup.exe"
+set "TARGET_DIR=\\ad.npixel.co.kr\share\1_Setup\34_ExcelMerge"
+
+robocopy "%SOURCE_DIR%" "%TARGET_DIR%" "%FILE_NAME%" /R:2
+
+IF %ERRORLEVEL% GEQ 8 (
+    echo Copy Fail! (Code: %ERRORLEVEL%)
+    exit /b 1
+) else (
+    echo Copy Success! (Code: %ERRORLEVEL%)
+    exit /b 0
+)
