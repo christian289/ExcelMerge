438e29a3e13c195ba6798d91c71dbd146a2bdd24
Vincent <christian289@naver.com>
2025-04-09 19:04:03 +0900

    Update Transfer-Installer.bat
    젠킨스 job의 실패 성공 여부 수정

--- Transfer-Installer.bat
+++ Transfer-Installer.bat
@@ -6,10 +6,9 @@ set "TARGET_DIR=\\ad.npixel.co.kr\share\1_Setup\34_ExcelMerge"
 
 robocopy "%SOURCE_DIR%" "%TARGET_DIR%" "%FILE_NAME%" /R:2
 
-IF %ERRORLEVEL% GEQ 8 (
-    echo Copy Fail! (Code: %ERRORLEVEL%)
-    exit /b 1
-) else (
-    echo Copy Success! (Code: %ERRORLEVEL%)
-    exit /b 0
+IF %errorlevel% NEQ 1 (
+   echo Copy Fail! (Code: %ERRORLEVEL%)
+   exit %errorlevel%
 )
+
+exit 0
